b5195d637705e168be01ff8b02b63ea975b5896f
xluBU <xlu@bournemouth.ac.uk>
2021-11-13 00:28:45 +0000

    Week8 testing

---
+++ Week8/pytest/Helloworld/main_test.py
@@ -0,0 +1,10 @@
+from Unittests.Pytestexample.Helloworld import main
+
+
+def test_index():
+    main.app.testing = True
+    client = main.app.test_client()
+
+    r = client.get('/')
+    assert r.status_code == 200
+    assert 'Hello World' in r.data.decode('utf-8')
